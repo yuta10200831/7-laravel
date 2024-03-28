2ef97da484ab5e6361a54d01aaf9e5eab35126e6
yuta10200831 <yfuse20ki@yahoo.co.jp>
2024-03-29 00:12:31 +0900

    お気に入り機能の実装

---
+++ laravel-project/database/migrations/2024_03_28_234741_remove_autoincrement_from_favorites_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class RemoveAutoincrementFromFavoritesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('favorites', function (Blueprint $table) {
+            $table->bigInteger('id', true, true)->change();
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('favorites', function (Blueprint $table) {
+            $table->bigIncrements('id')->change();
+        });
+    }
+}
